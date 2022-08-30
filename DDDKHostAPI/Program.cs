a60cf87b40adca17ac0ddeeea01d2811414cdeed
Aleksandar Matic <aleksbn417@hotmail.com>
2022-08-30 12:51:02 +0200

    Turned SWAGGER on

--- DDDKHostAPI/Program.cs
+++ DDDKHostAPI/Program.cs
@@ -67,11 +67,14 @@ builder.Services.AddControllers().AddNewtonsoftJson(o => o.SerializerSettings.Re
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.ConfigureExceptionHandling();
 
